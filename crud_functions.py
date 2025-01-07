1436eb27f53fdcf6e0ec5a1f4747dadcbec17ba1
Grimbet <raw101081@gmail.com>
2025-01-07 21:21:24 +0300

    Файл для работы с БД для module_14_4 и 14_5

--- crud_functions.py
+++ crud_functions.py
@@ -3,8 +3,8 @@ import sqlite3
 import os
 
 # создаем файл БД, если его нет
-if not os.path.exists('products.db'):
-    my_file = open("products.db", "w+")
+if not os.path.exists('database.db'):
+    my_file = open("database.db", "w+")
     my_file.close()
 
 # заполнение продуктами БД
@@ -19,11 +19,11 @@ def zap_db(con):
 
 # подключение к БД
 def connect_db():
-    connection = sqlite3.connect('products.db')
+    connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
     return connection, cursor
 
-# создает таблицу Products
+# создаем таблицы Products и Users
 def initiate_db(conn):
     conn[1].execute('''
     CREATE TABLE IF NOT EXISTS Products(
@@ -33,12 +33,37 @@ def initiate_db(conn):
     price INTEGER NOT NULL    
     )
     ''')
+    conn[1].execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL    
+    )
+    ''')
 
 # выводит все продукты из БД
 def get_all_products(conn):
     conn[1].execute("SELECT * FROM Products ")
     products = conn[1].fetchall()
     return products
+
+# выводит если username есть в БД
+def is_included(conn,username):
+    conn[1].execute("SELECT id FROM Users WHERE username=?", (username,))
+    data = conn[1].fetchone()
+    if data is None:
+        return False
+    else:
+        return True
+
+# добавить в БД нового user
+def add_user(conn, username, email, age):
+    if not is_included(conn,username):
+        conn[1].execute("INSERT INTO Users(username,email,age,balance) VALUES(?,?,?,?)",
+                    (username,email,age,1000))
+        conn[0].commit()
 
 
 if __name__ == '__main__':
@@ -48,5 +73,6 @@ if __name__ == '__main__':
     products = get_all_products(conn) # выводим эти данные
     for product in products:
         print(f"Название: {product[1]} | Описание: {product[2]} | Цена: {product[3]}")
+
     conn[0].commit() # сохраняемся
     conn[0].close() # выходим
